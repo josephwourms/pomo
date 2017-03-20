8f3b8d3d1d0096c1d9262508b988801b218baeb7
josephwourms <joseph.wourms@gmail.com>
2017-03-20 15:08:20 -0300

    seperate break-slider into component

---
+++ pomo-mobile/www/js/components/break-slider/break-slider.component.js
@@ -0,0 +1,5 @@
+angular.module('pomoApp')
+  .component('breakSlider', {
+    templateUrl: '/js/components/break-slider/break-slider.template.html',
+    controller: 'breakSliderCtrl'
+  })
---
+++ pomo-mobile/www/js/components/break-slider/break-slider.controller.js
@@ -0,0 +1,6 @@
+angular.module('pomoApp') 
+  .controller('breakSliderCtrl', [function() {
+    var ctrl = this;
+
+    ctrl.breakTime = 300;
+  }])
