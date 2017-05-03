60e5e17c7fff2fe09a65895ca62857f89e3179e2
Joseph Wourms <joseph.wourms@gmail.com>
2017-05-03 01:32:25 +0000

    change green color

--- pomo-app/src/components/TimeDisplay.js
+++ pomo-app/src/components/TimeDisplay.js
@@ -3,26 +3,27 @@ import Paper from 'material-ui/Paper';
 
 import './TimeDisplay.css';
 
-const WIDTH = screen.width * 0.60;
+const WIDTH = (window.innerWidth < 0) ? screen.width * 0.60 : window.innerWidth / 2;
 const HEIGHT = WIDTH;
 
 class TimeDisplay extends Component {
     constructor(props) {
         super(props);
-        this.onClick = this.onClick.bind(this);
         
         this.style = {
             height: HEIGHT,
             width: WIDTH,
+            lineHeight: 0,
             margin: 20,
             textAlign: 'center',
             verticalAlign: 'center',
             display: 'inline-block',
             background: `linear-gradient(to top, #5a9216 ${this.props.percentDone}%, #ba000d 0%)`,
             color: 'rgba(255, 255, 255, 0.8)',
-            
+            cursor: 'pointer',
         };
         
+        this.onClick = this.onClick.bind(this);
     }
     
     onClick(e) {
@@ -35,16 +36,16 @@ class TimeDisplay extends Component {
         
         if (this.props.status === 'Work Session') {
             c1 = "#ba000d";
-            c2 = "#5a9216";
+            c2 = "#8bc34a";
         } else {
-            c1 = "#5a9216";
+            c1 = "#8bc34a";
             c2 = "#ba000d";
         }
         this.style.background = `linear-gradient(to top, ${c1} ${this.props.percentDone}%, ${c2} 0%)`;
         
         return (
             <div className="time-display">
-                <Paper style={this.style} zDepth={2} circle={true} onClick={this.onClick}>
+                <Paper style={this.style} zDepth={1} circle={true} onClick={this.onClick}>
                     <div className="time-display inner">
                         <p className="time-display status">{this.props.status}</p>
                         <h3 className="time-display time">{this.props.time}</h3>
